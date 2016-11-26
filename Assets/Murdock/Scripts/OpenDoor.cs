b2c09da5e54d1af49bdd88ef29f4a00f86e27546
Ivar-TOSH\Ivar <ivarbjorling@hotmail.com>
2016-11-26 11:46:20 +0100

    Open Door script (put on door.fbx)

---
+++ Assets/Murdock/Scripts/OpenDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+    private float timeToOpen = 6.4f;
+    private float timer = 0.0f;
+    private float maxRotation = 90.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timer < timeToOpen)
+        {
+            timer += Time.deltaTime;
+
+            float rotation = timer / timeToOpen * maxRotation;
+
+            this.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+	}
+}
