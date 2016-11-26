aa7a33c16ac2869354dc137e82e64356a2a3c7b1
Trimblewick <gustav.ekelund@hotmail.com>
2016-11-26 15:39:45 +0100

    Phone and door

--- Assets/Murdock/Scripts/OpenDoor.cs
+++ Assets/Murdock/Scripts/OpenDoor.cs
@@ -3,22 +3,31 @@ using System.Collections;
 
 public class OpenDoor : MonoBehaviour {
 
+    private float delay = 8;
     private float timeToOpen = 6.55f;
     private float timer = 0.0f;
     private float maxRotation = 90.0f;
+    private bool firstTime = true;
 
     public AudioSource openDoorSound;
 
 	// Use this for initialization
 	void Start () {
-        openDoorSound.Play();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (timer < timeToOpen)
+        delay -= Time.deltaTime;
+        
+        if (delay < 0 && timer < timeToOpen)
         {
+            if (firstTime)
+            {
+                openDoorSound.Play();
+                firstTime = false;
+            }
+
             timer += Time.deltaTime;
 
             float rotation = timer / timeToOpen * maxRotation;
