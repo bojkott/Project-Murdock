924da20454ac0f55219e61cdb38e2ae8e66e75be
Ivar-TOSH\Ivar <ivarbjorling@hotmail.com>
2016-11-26 12:06:38 +0100

    Door prefab

--- Assets/Murdock/Scripts/OpenDoor.cs
+++ Assets/Murdock/Scripts/OpenDoor.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class OpenDoor : MonoBehaviour {
 
-    private float timeToOpen = 6.4f;
+    private float timeToOpen = 6.55f;
     private float timer = 0.0f;
     private float maxRotation = 90.0f;
 
+    public AudioSource openDoorSound;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        openDoorSound.Play();
+    }
 	
 	// Update is called once per frame
 	void Update () {
