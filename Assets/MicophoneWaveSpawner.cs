bb60b8a8f3d4fd41af708551730baad2570e2cbc
Pontus Björkberg <pontus.bjorkberg@hotmail.com>
2016-11-26 16:47:55 +0100

    Finished stage

--- Assets/MicophoneWaveSpawner.cs
+++ Assets/MicophoneWaveSpawner.cs
@@ -1,101 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MicophoneWaveSpawner : MonoBehaviour {
+public class MicophoneWaveSpawner : MonoBehaviour
+{
 
-    public float mic_distance = 1;
-
-    public float interval;
-    public Transform origin1;
-    public Transform origin2;
-
-    public float saturation = 0.5f;
-    public float vibrance = 1.0f;
-
-    public int samples = 1024;
-    public float sensitivity = 100;
-    public int freqs = 128;
-    public FFTWindow fftWindow;
-    public float loudness = 0;
     private AudioSource _audio;
-    private WaveManager wm;
-    private float time;
 
     void Awake()
     {
         _audio = GetComponent<AudioSource>();
+
     }
     void Start()
     {
-        wm = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<WaveManager>();
         _audio.clip = Microphone.Start("Built-in Microphone", true, 1, 44100);
 
     }
     void Update()
     {
 
-        if(Vector3.Distance(wm.transform.position, transform.position) < mic_distance)
+        if (_audio.isPlaying)
         {
 
-            if (_audio.isPlaying)
-            {
-
-            }
-            else if (_audio.clip.isReadyToPlay)
-                _audio.Play();
-            else
-            {
-                _audio.clip = Microphone.Start("Built-in Microphone", true, 1, 44100);
-            }
-
-            time += Time.deltaTime;
-            loudness = GetAveragedVolume() * sensitivity;
-            if (loudness > 1 && time > interval)
-            {
-                float h = 0;
-                float waveSpeed = 0;
-                float[] spectrum = _audio.GetSpectrumData(samples, 0, fftWindow);
-
-                for (int i = 0; i < freqs; i++)
-                {
-                    h += spectrum[i];
-                    waveSpeed += spectrum[i] * i;
-                }
-
-
-
-                Color color = Color.HSVToRGB(h, 0.5f, 1.0f);
-
-                if (wm != null)
-                {
-                    loudness = Mathf.Clamp(loudness, 1, 3);
-                    Vector3 spawnPos = transform.position;
-                    if (origin1 != null)
-                    {
-                        wm.CreateWave(origin1.position, waveSpeed, color, loudness, 0);
-                        wm.CreateWave(origin2.position, waveSpeed, color, loudness, 0);
-                    }
-
-                }
-
-                time = 0;
-            }
-
+        }
+        else if (_audio.clip.isReadyToPlay)
+            _audio.Play();
+        else
+        {
+            _audio.clip = Microphone.Start("Built-in Microphone", true, 1, 44100);
         }
 
     }
-
-    float GetAveragedVolume()
-    {
-        float[] data = new float[256];
-        float a = 0;
-        _audio.GetOutputData(data, 0);
-        foreach (float s in data)
-        {
-            a += Mathf.Abs(s);
-        }
-        return a / 256;
-    }
-
-
 }
